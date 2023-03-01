78f6b80bf7d0a00d0436a6aaa9e842e6ada5d263
Adrian Salt <56944403+AdrianS-commits@users.noreply.github.com>
2023-03-01 00:33:04 +0000

    Working for all week 6 activities

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def create_app():
     """Create and configure the Flask app"""
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = 'Wd-oxH5_zAOO6qveNAQiQg'
 
     # Include the routes from app.py
     with app.app_context():
--- flask_app/hello.py
+++ flask_app/hello.py
@@ -1,8 +1,9 @@
 from flask import current_app as app
+from flask import render_template
 
 # Remove this and the subsequent line, left in to show what was here before the Factory Application pattern was applied
 # app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
