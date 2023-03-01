c22034f98848e5929a8057db116027e34bfedc6c
Adrian Salt <56944403+AdrianS-commits@users.noreply.github.com>
2023-03-01 00:51:14 +0000

    Week 6 fully working with Blueprinting

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,6 +4,18 @@ from flask import Flask
 def create_app():
     """Create and configure the Flask app"""
     app = Flask(__name__)
+
+    # Registering the blueprint before returning the Flask app object.
+    from flask_app.hello import main_bp
+    app.register_blueprint(main_bp)
+
+    
+#     In order to generate a secret key, enter the following in terminal:
+#     python
+#       >> import secrets
+#       >> secrets.token_urlsafe(16)
+
+
     app.config["SECRET_KEY"] = 'Wd-oxH5_zAOO6qveNAQiQg'
 
     # Include the routes from app.py
--- flask_app/hello.py
+++ flask_app/hello.py
@@ -1,9 +1,18 @@
-from flask import current_app as app
-from flask import render_template
+# from flask import current_app as app
+from flask import render_template, Blueprint
 
 # Remove this and the subsequent line, left in to show what was here before the Factory Application pattern was applied
 # app = Flask(__name__)
 
-@app.route("/")
+# Now creating a blueprint
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route("/")
 def index():
     return render_template("index.html")
+
+
+
+
+# You need to enter the following in a terminal window for it to work:
+# flask --app 'flask_app:create_app()' --debug run
